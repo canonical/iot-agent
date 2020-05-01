2fa6b4e9346111f5e15f32f2c5da80615d959cf3
James Jesudason <james.jesudason@gmail.com>
2020-05-01 15:58:22 +0100

    Add metrics logging to MQTT

--- mqtt/metrics.go
+++ mqtt/metrics.go
@@ -21,16 +21,49 @@ package mqtt
 
 import (
 	"fmt"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 	"log"
-	"math/rand"
 )
 
 // Metrics publishes a metrics messages to indicate so the device can be monitored
 func (c *Connection) Metrics() {
-	payload := fmt.Sprintf("metrics,device=%s cpu=%f,mem=%d", c.clientID, rand.Float64()*100, rand.Intn(2000))
-	log.Println(payload)
+	// Publish the stats
+	c.memory()
+	c.cpu()
+}
 
+func (c *Connection) publishMetrics(payload string) {
 	// The topic to publish the response to the specific action
 	t := fmt.Sprintf("metrics/%s", c.organisationID)
 	c.client.Publish(t, QOSAtMostOnce, false, []byte(payload))
+}
+
+func (c *Connection) memory() {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		log.Printf("Error getting memory usage: %v\n", err)
+		return
+	}
+
+	payload := fmt.Sprintf("memory,device=%s total=%d,used=%d,usedpc=%f", c.clientID, v.Total, v.Used, v.UsedPercent)
+	c.publishMetrics(payload)
+}
+
+func (c *Connection) cpu() {
+	vv, err := cpu.Times(false)
+	if err != nil {
+		log.Printf("Error getting cpu usage: %v\n", err)
+		return
+	}
+
+	var user, system, total float64
+	for _, v := range vv {
+		user += v.User
+		system += v.System
+		total += v.Total()
+	}
+
+	payload := fmt.Sprintf("cpu,device=%s user=%f,system=%f,total=%f", c.clientID, user, system, total)
+	c.publishMetrics(payload)
 }
